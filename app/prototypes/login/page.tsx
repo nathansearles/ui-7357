304825737687a824a083662543b4ce7a4b906914
Nathan Searles <nsearles@gmail.com>
2026-05-26 12:16:18 -0700

    Update page.tsx

--- app/prototypes/login/page.tsx
+++ app/prototypes/login/page.tsx
@@ -12,7 +12,7 @@ export default function LoginPage() {
           <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
             <CardsThreeIcon className="size-4" />
           </div>
-          Acme Inc.
+          Awesome Inc.
         </a>
         <LoginForm />
       </div>
